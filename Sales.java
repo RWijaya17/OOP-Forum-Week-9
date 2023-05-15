3b8ac6618589b7b0840fcae5d0ade56fc8e9f0bb
RWijaya17 <122419691+RWijaya17@users.noreply.github.com>
2023-05-15 12:15:05 +0700

    Update Sales.java

--- Sales.java
+++ Sales.java
@@ -1,1 +1,24 @@
+package ForumWeek9;
 
+public class Sales {
+
+    // each object contains details of one sale
+
+    private String itemId;     // id of the item
+
+    private double value;      // the price of one item
+
+    private int quantity;      // the number of the items sold
+
+    // constructor
+    public Sales(String id, double value, int quantity){
+        this.itemId = id;
+        this.value = value;
+        this.quantity = quantity;
+    }
+
+    public double getValue() {return value;}
+
+    public int getQuantity() {return quantity;}
+
+}
